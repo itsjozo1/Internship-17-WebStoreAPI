1cebfbb5d59b8a04ff2d429f88a5897a39156caf
rokoperisic <roko.perisic@gmail.com>
2024-04-04 17:41:10 +0200

    Deleted backend folders form main

--- src/wishlists/dto/create-wishlist.dto.ts
+++
@@ -1,6 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-
-export class CreateWishlistDto {
-  @ApiProperty()
-  userId: number;
-}
--- src/wishlists/entities/wishlist.entity.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { Wishlist } from '@prisma/client';
-
-export class WishlistEntity implements Wishlist {
-  @ApiProperty()
-  id: number;
-  @ApiProperty()
-  userId: number;
-}
