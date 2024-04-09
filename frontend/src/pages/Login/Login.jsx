f6cb58d680dc9462b5671d06b8981d27a2aadfa7
rokoperisic <roko.perisic@gmail.com>
2024-04-09 19:09:13 +0200

    Updated login page

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,14 +1,28 @@
+import classes from './index.module.css';
+import Header from '../../components/Header/Header';
+
 function Login() {
+  const handleClose = () => {
+    window.location.href = '/';
+  };
+
   return (
-    <div>
-      <h1>Login</h1>
-      <div>
-        <span>Email</span>
-        <input type="email" id="email" />
-        <span>Password</span>
-        <input type="password" id="password" />
+    <>
+      <Header />
+      <div className={classes.loginContainer}>
+        <button className={classes.closeButton} onClick={handleClose}>
+          x
+        </button>
+        <h1>Login</h1>
+        <div className={classes.loginInputContainer}>
+          <span>Email</span>
+          <input type="email" id="email" />
+          <span>Password</span>
+          <input type="password" id="password" />
+        </div>
+        <button className={classes.loginButton}>Login</button>
       </div>
-    </div>
+    </>
   );
 }
 
